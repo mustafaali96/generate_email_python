e9a657091e21139cdf19e74ea940ca96a891b5b2
Mustafa Ali <mustafa_socool@hotmail.com>
2020-09-19 10:57:15 -0700

    attach files code

--- send_email.py
+++ send_email.py
@@ -1,19 +1,30 @@
 import smtplib, ssl
+from email.mime.multipart import MIMEMultipart
+from email.mime.image import MIMEImage
+from email.mime.text import MIMEText
 
 port = 587  # For starttls
-smtp_server = "smtp.gmail.com"
+smtp_server = "smtp.gmail.com"  #'smtp.office365.com'
 sender_email = "sender@mail.com"
 receiver_email = "receiver@mail.com"
-password = input("Type your password and press enter:")
-message = """\
-Subject: Hi there
+password = "16-character code generated from App Password" #input("Type your password and press enter:")
+msg = MIMEMultipart()
+msg['Subject'] = 'Testing email with attachment'
+msg['From'] = sender_email
+msg['To'] = receiver_email 
 
-This message is sent from Python."""
+msgText = MIMEText('<b>%s</b>' % ("This is the body of email"), 'html')
+msg.attach(msgText)
 
-context = ssl.create_default_context()
+with open('python_image.png', 'rb') as fp:
+        img = MIMEImage(fp.read())
+        img.add_header('Content-Disposition', 'attachment', filename="python_image.png")
+        msg.attach(img)
+
+#context = ssl.create_default_context()
 with smtplib.SMTP(smtp_server, port) as server:
     server.ehlo()  # Can be omitted
-    server.starttls(context=context)
+    server.starttls()
     server.ehlo()  # Can be omitted
     server.login(sender_email, password)
-    server.sendmail(sender_email, receiver_email, message)
+    server.sendmail(sender_email, receiver_email, msg.as_string())
